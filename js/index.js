74bf09f1a622c1beb39a83baecb2ac4e85324948
octaviolan <octavio.olan25@gmail.com>
2024-02-25 20:18:37 -0600

    Card desktop

--- js/index.js
+++ js/index.js
@@ -2,30 +2,31 @@ const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*
 const input = document.querySelector('.form-input');
 const buttonSubscribe = document.querySelector('.card-form .button');
 const modal = document.querySelector('.modal');
-const buttonDismiss = document.querySelector('.modal .button')
-const error = document.querySelector('.email-invalid');
-const confirmation = document.querySelector('.modal .confirmation')
+const buttonDismiss = document.querySelector('.modal .button');
+const errorMessage = document.querySelector('.email-invalid');
+const emailConfirmation = document.querySelector('.modal .confirmation')
 
 
 buttonSubscribe.addEventListener('click', ()=> {
 
   if(pattern.test(input.value)) {
-    console.log(input.value);
-    confirmation.textContent = input.value;
+    emailConfirmation.textContent = input.value;
     modal.classList.add('show')
+    document.documentElement.scrollTop = 0;
   } else {
-    error.classList.add('show');
+    errorMessage.classList.add('show');
     input.classList.add('form-input-isRed');
     setTimeout(()=>{
-      error.classList.remove('show');
-      input.classList.remove('form-input-isRed')
-    }, 2000)
+      errorMessage.classList.remove('show');
+      input.classList.remove('form-input-isRed');
+    }, 1500)
   }
 });
 
 buttonDismiss.addEventListener('click', ()=> {
   modal.classList.remove('show');
   input.value = '';
+  document.documentElement.scrollTop = 0;
 }) 
 
 
